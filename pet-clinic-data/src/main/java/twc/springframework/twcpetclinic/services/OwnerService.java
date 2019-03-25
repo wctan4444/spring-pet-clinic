b9d8ca70fde7b54033b77155640424a52f6d7e5d
waichong <wctan4444@yahoo.com>
2019-03-25 21:42:16 +0800

    Implement Interfaces for services

---
+++ pet-clinic-data/src/main/java/twc/springframework/twcpetclinic/services/OwnerService.java
@@ -0,0 +1,17 @@
+package twc.springframework.twcpetclinic.services;
+
+import twc.springframework.twcpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+}
