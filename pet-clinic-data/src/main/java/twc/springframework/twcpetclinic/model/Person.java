55b03cf33940fd5bd36d7b7320ed788e81d257cc
waichong <wctan4444@yahoo.com>
2019-03-25 21:50:13 +0800

    Add Base Entity

--- pet-clinic-data/src/main/java/twc/springframework/twcpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/twc/springframework/twcpetclinic/model/Person.java
@@ -1,6 +1,7 @@
 package twc.springframework.twcpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
+
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/twc/springframework/twcpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/twc/springframework/twcpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package twc.springframework.twcpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
