ac049ee579a73dfbb3ea9cc0d6ce3f372c882f04
waichong <wctan4444@yahoo.com>
2019-03-26 22:02:26 +0800

    Implement Map based Services

---
+++ pet-clinic-data/src/main/java/twc/springframework/twcpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package twc.springframework.twcpetclinic.services.map;
+
+import twc.springframework.twcpetclinic.model.Pet;
+import twc.springframework.twcpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById();
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
