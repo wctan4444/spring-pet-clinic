c7733f4e6a59323388939c2c39c41b1a64b111bb
waichong <wctan4444@yahoo.com>
2019-04-06 12:06:12 +0800

    Add Owners List and updated Controller.

--- pet-clinic-web/src/main/java/twc/springframework/twcpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/twc/springframework/twcpetclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package twc.springframework.twcpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import twc.springframework.twcpetclinic.services.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners",ownerService.findAll());
 
         return "/owners/index";
     }
